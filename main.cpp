466ce5b3ee9c632b5bb19e0c852379e9699860d6
gyuwon <a1415@hansung.ac.kr>
2026-08-10 23:45:15 +0900

    pre-integration feedback+

--- main.cpp
+++ main.cpp
@@ -19,21 +19,29 @@ using namespace std;
 #define VOLX 256
 #define VOLY 256	
 #define VOLZ 225
+const int BSIZE = 8;
+const int BSHIFT = 3;
 
+unsigned char ImageBuf[HEIGHT][WIDTH];
 unsigned char MyTexture[HEIGHT][WIDTH][3];
 unsigned char vol[VOLZ][VOLY][VOLX];
 
+//수정 이전
+const int BZ_COUNT = VOLZ / BSIZE + (VOLZ % BSIZE != 0); // 29
+const int BY_COUNT = VOLY / BSIZE + (VOLY % BSIZE != 0); // 32
+const int BX_COUNT = VOLX / BSIZE + (VOLX % BSIZE != 0); // 32
+unsigned char bM[BZ_COUNT][BY_COUNT][BX_COUNT];
+unsigned char bm[BZ_COUNT][BY_COUNT][BX_COUNT];
+
 float alphaTable[256];
+float sumTable[256];
 float colorTableR[256];
 float colorTableG[256];
 float colorTableB[256];
 
 //pre-integration
-#define DEFAULT_SEGMENT_LENGTH 0.5f   // 기존 코드의 step=0.5 와 동일
+#define DEFAULT_SEGMENT_LENGTH 0.5f   // 기존 코드의 step=0.5 와 동일 값 (호환성)
 #define TABLE_SIZE 256         
-// 알파, 컬러의 적분
-float preT[TABLE_SIZE];
-float preKr[TABLE_SIZE], preKg[TABLE_SIZE], preKb[TABLE_SIZE];
 // 좌표축이 (sf, sb) 두 개인 2D 테이블. CPU에서 만든 뒤 GPU로 1회 업로드.
 // 배열은 1차원으로
 float preAlpha[TABLE_SIZE * TABLE_SIZE];
@@ -143,6 +151,56 @@ void FileRead()
 	myfile.close();
 }
 
+inline bool isOutside(const glm::vec3& p) {//범위 처리 따라, 알파 컬러에서는 불필요
+	if (p.x >= VOLX || p.x < 0 ||
+		p.y >= VOLY || p.y < 0 ||
+		p.z >= VOLZ || p.z < 0) return true;
+	else
+		return false;
+}
+
+void GenBlocks() { //수정A-2: 29, 32, 32에서 각각 B~_COUNT
+	for (int bz = 0; bz < BZ_COUNT; bz++) // BZ = 28 
+		for (int by = 0; by < BY_COUNT; by++)
+			for (int bx = 0; bx < BX_COUNT; bx++) { // 각 블록에 대해서
+				unsigned char max_value = 0, min_value = 255;
+				// 최대값을 추출해서 //(개선+; 경계값 추가)
+				for (int z = bz * BSIZE; z <= __min(bz * BSIZE + BSIZE, VOLZ - 1); z++) { // 28*8 = for 224      z<232      vol[226]
+					for (int y = by * BSIZE; y <= __min(by * BSIZE + BSIZE, VOLY - 1); y++) {
+						for (int x = bx * BSIZE; x <= __min(bx * BSIZE + BSIZE, VOLX - 1); x++) { //bx=31, 31*8=248~256
+							max_value = __max(vol[z][y][x], max_value);
+							min_value = __min(vol[z][y][x], min_value);
+						}
+					}
+				}
+				// 저장한다.
+				bM[bz][by][bx] = max_value;
+				bm[bz][by][bx] = min_value;
+			}
+	printf("max = %d, min = %d \n", bM[14][16][16], bm[14][16][16]);
+}
+
+int GetDensity(glm::vec3 p) {
+	int ix = int(p.x); // 4.8 ->  4
+	int iy = int(p.y); // 4.8 ->  4
+	int iz = int(p.z); // 4.8 ->  4
+	float wx = p.x - ix;
+	float wy = p.y - iy;
+	float wz = p.z - iz;
+	// y,z
+	// linear interpolation : 직선형 보간
+	// cubic interpolation : 3차 함수를 이용한 보간
+	//000	001 010 011 100 101 110 111
+	int den = vol[iz][iy][ix] * (1 - wx) * (1 - wy) * (1 - wz)
+		+ vol[iz][iy][ix + 1] * (wx) * (1 - wy) * (1 - wz)
+		+ vol[iz][iy + 1][ix] * (1 - wx) * (wy) * (1 - wz)
+		+ vol[iz][iy + 1][ix + 1] * (wx) * (wy) * (1 - wz)
+		+ vol[iz + 1][iy][ix] * (1 - wx) * (1 - wy) * (wz)
+		+vol[iz + 1][iy][ix + 1] * (wx) * (1 - wy) * (wz)
+		+vol[iz + 1][iy + 1][ix] * (1 - wx) * (wy) * (wz)
+		+vol[iz + 1][iy + 1][ix + 1] * (wx) * (wy) * (wz);
+	return den;
+}
 
 void InitTables() {
 	AlphaTable  mat;
@@ -154,6 +212,14 @@ void InitTables() {
 	mat.AddPoint(255, 1.0f);
 	mat.MakeAlphaTable(alphaTable);
 
+	sumTable[0] = alphaTable[0];
+	for (int i = 1; i < 256; i++) {
+		sumTable[i] = sumTable[i - 1] + alphaTable[i];
+	}
+
+	//in : alphaTable
+	//out : sumTable
+
 	ColorTable ct;
 	ct.AddPoint(0, 0.0f, 0.0f, 0.0f);
 	ct.AddPoint(120, 0.1f, 0.2f, 0.55f);   // 딥블루 오팔베이스
@@ -164,59 +230,189 @@ void InitTables() {
 	ct.MakeColorTable(colorTableR, colorTableG, colorTableB);
 }
 
+int inline GetBlockId(glm::vec3 p) {//(개선+); 시프트 연산자로 블록 아이디 계산
+	int x = p.x, y = p.y, z = p.z;
+	int bx = x >> BSHIFT, by = y >> BSHIFT, bz = z >> BSHIFT;
+	return (bx << 10) | (by << 5) | bz; // 수정A-4: 시프트 복호화로(어차피 진수표현만 상이)
+}
 void BuildPreIntegrationTable(float segmentLength) {
-	//누적 적분
-	preT[0] = 0.0f;
-	preKr[0] = preKg[0] = preKb[0] = 0.0f;
-	for (int s = 1; s < TABLE_SIZE; s++) {
-		// preT:alphaTable
-		preT[s] = preT[s - 1] + 0.5f * (alphaTable[s - 1] + alphaTable[s]);
-		// preK:associated color의 누적
-		float ar0 = colorTableR[s - 1] * alphaTable[s - 1];
-		float ar1 = colorTableR[s] * alphaTable[s];
-		float ag0 = colorTableG[s - 1] * alphaTable[s - 1];
-		float ag1 = colorTableG[s] * alphaTable[s];
-		float ab0 = colorTableB[s - 1] * alphaTable[s - 1];
-		float ab1 = colorTableB[s] * alphaTable[s];
-		preKr[s] = preKr[s - 1] + 0.5f * (ar0 + ar1);
-		preKg[s] = preKg[s - 1] + 0.5f * (ag0 + ag1);
-		preKb[s] = preKb[s - 1] + 0.5f * (ab0 + ab1);
-	}
+	// 구간 [sf, sb] 를 |sb-sf| 개의 부분샘플로 쪼개서 실제로 알파블렌딩(front-to-back)한다.
+	// 구간 내 밀도는 sf -> sb 로 선형변화하므로, 루프변수 i 자체가 선형보간된 밀도가 된다.
+	// (지수/누적적분 근사가 아니라, 각 부분샘플의 산술적인 겹침을 그대로 합성)
+	(void)segmentLength; // 알파보정은 1/width 기준이므로 여기서는 쓰지 않음
 
-	//2차원 테이블
-	for (int sf = 0; sf < TABLE_SIZE; sf++) {
-		for (int sb = 0; sb < TABLE_SIZE; sb++) {
+	for (int sf = 0; sf < TABLE_SIZE; sf++) {          // front density
+		for (int sb = 0; sb < TABLE_SIZE; sb++) {      // back density
 			int idx = sf * TABLE_SIZE + sb;
-			float tau_avg, cr_avg, cg_avg, cb_avg;
 
-			if (sf == sb) {//알파값은 대칭 최적화 해도 될듯?
-				tau_avg = alphaTable[sf];
-				cr_avg = colorTableR[sf] * alphaTable[sf];
-				cg_avg = colorTableG[sf] * alphaTable[sf];
-				cb_avg = colorTableB[sf] * alphaTable[sf];
+			float r_sum = 0.0f, g_sum = 0.0f, b_sum = 0.0f, a_sum = 0.0f;
+
+			int n = (sb > sf) ? (sb - sf) : (sf - sb);  // 부분샘플 개수
+			if (n == 0) {
+				// 밀도 변화가 없는 구간: 부분샘플 1개짜리와 동일 (보정 지수 = 1)
+				float a = alphaTable[sf];
+				a_sum = a;
+				r_sum = colorTableR[sf] * a;
+				g_sum = colorTableG[sf] * a;
+				b_sum = colorTableB[sf] * a;
 			}
 			else {
-				float inv_diff = 1.0f / (float)(sb - sf);
-				tau_avg = (preT[sb] - preT[sf]) * inv_diff;
-				cr_avg = (preKr[sb] - preKr[sf]) * inv_diff;
-				cg_avg = (preKg[sb] - preKg[sf]) * inv_diff;
-				cb_avg = (preKb[sb] - preKb[sf]) * inv_diff;
+				float width = (float)n;
+				float inv_width = 1.0f / width;             // 부분샘플 하나의 두께
+				int dir = (sb > sf) ? 1 : -1;               // 광선 진행 방향(sf -> sb) 유지
+
+				for (int i = sf; i != sb; i += dir) {
+					float alpha = alphaTable[i];
+					if (alpha > 0.0f) {
+						// alpha-correction : 얇아진 두께(1/width)에 맞춰 보정
+						float ca = 1.0f - powf(1.0f - alpha, inv_width);
+
+						// front-to-back alpha blending (associated color)
+						r_sum += (1.0f - a_sum) * colorTableR[i] * ca;
+						g_sum += (1.0f - a_sum) * colorTableG[i] * ca;
+						b_sum += (1.0f - a_sum) * colorTableB[i] * ca;
+						a_sum += (1.0f - a_sum) * ca;
+
+						if (a_sum > 0.9999f) break;         // 구간 내 조기 종료
+					}
+				}
 			}
 
-			float alphaVal = 1.0f - expf(-segmentLength * tau_avg);
-			preAlpha[idx] = alphaVal;
-			// 흡수 무시 근사: C ≈ c_avg * d
-			preColorR[idx] = cr_avg * segmentLength;
-			preColorG[idx] = cg_avg * segmentLength;
-			preColorB[idx] = cb_avg * segmentLength;
+			// 한 구간을 통째로 합성한 결과를 저장 (커널의 사용 방식과 동일한 계약)
+			preAlpha[idx] = a_sum;
+			preColorR[idx] = r_sum;
+			preColorG[idx] = g_sum;
+			preColorB[idx] = b_sum;
 		}
 	}
 }
 
+void Render(glm::vec3 eye) {
+	using namespace glm;
+
+	glm::vec3 at(128, 128, 112);
+	glm::vec3 up(0, 1, 0);
+
+	glm::vec3 w = glm::normalize(at - eye);
+	glm::vec3 u = glm::normalize(glm::cross(up, w));
+	glm::vec3 v = glm::normalize(glm::cross(w, u));
+
+
+	auto start = std::chrono::high_resolution_clock::now();
+	const float supersampling = 0.5f * (512.0f / WIDTH);
+	/////////////////레이캐스팅
+	for (int y = 0; y < HEIGHT; y++) { // 영상의 y좌표
+		for (int x = 0; x < WIDTH; x++) { // 영상의 x좌표
+			glm::vec3 RS = eye + u * (x - WIDTH * 0.5f) * supersampling + v * (y - HEIGHT * 0.5f) * supersampling;
+
+			float t1, t2; // 한 구간
+			t1 = -RS.x / w.x;
+			t2 = (255 - RS.x) / w.x;
+			float xm = __min(t1, t2);
+			float xM = __max(t1, t2);
+
+			t1 = -RS.y / w.y;
+			t2 = (255 - RS.y) / w.y;
+			float ym = __min(t1, t2);
+			float yM = __max(t1, t2);
+
+			t1 = -RS.z / w.z;
+			t2 = (224 - RS.z) / w.z;
+			float zm = __min(t1, t2);
+			float zM = __max(t1, t2);
+			float tm = __max(__max(xm, ym), zm);
+			float tM = __min(__min(xM, yM), zM);
+
+			float r_sum = 0.0f, g_sum = 0.0f, b_sum = 0.0f;
+			float a_sum = 0.0f;
+
+			const float step = 0.5;
+			for (float t = tm; t < tM; t = t + step) { // 광선을 진행하자
+				glm::vec3 p = RS + w * t;
+				if (isOutside(p))
+					continue;
+
+				// 내(p)가 속한 블록의 min, max 안다고 가정.
+				int bid = GetBlockId(p); // 123456
+				// 수정A-5: 비트연산자 활용해 봄. 
+				int bz = bid & 0x1F; //1F(16+15)임 즉, 11111이고 &연산함. (0x20-0x01해도?)             
+				int by = (bid >> 5) & 0x1F; // 5개 지우고 남은 오른쪽 5개 추출
+				int bx = (bid >> 10) & 0x1F; // 이하 동일
+				int min_value = bm[bz][by][bx];
+				int max_value = bM[bz][by][bx];
+				if (sumTable[max_value] - sumTable[min_value - 1] == 0) {
+					float jump = 0;
+					int nextBid;
+					// 투명한 블록임
+					//투명한 블록이니까, 연산을 건너뛰자. 광선을 빠르게 전진하자.
+					do {
+						jump += 1.0f;
+						nextBid = GetBlockId(p + w * jump); // 추가 전진
+					} while (bid == nextBid);
+					t = t + (jump - step);
+					continue;
+				}
+
+
+				int d = GetDensity(p); // vol[(int)(p.z)][int(p.y)][int(p.x)]; // d = x선 흡수도, d가 높으면 뼈 d가 낮으면 근육, 지방, 공기
+
+				float alpha = alphaTable[d]; // getAlpha(50, 200, d);
+				if (alpha == 0)
+					continue;
+				alpha = 1 - pow((1 - alpha), step); // alpha-correction
+
+
+				float r = colorTableR[d]; // d / 255.0;
+				float g = colorTableG[d];
+				float b = colorTableB[d];
+
+				//조명; 중앙차분법
+				float dx = (GetDensity(p + vec3(1, 0, 0)) - GetDensity(p - vec3(1, 0, 0))) * 0.5f; // 수정: 0.5 -> 0.5f (float 리터럴)
+				float dy = (GetDensity(p + vec3(0, 1, 0)) - GetDensity(p - vec3(0, 1, 0))) * 0.5f; // 수정: 0.5 -> 0.5f
+				float dz = (GetDensity(p + vec3(0, 0, 1)) - GetDensity(p - vec3(0, 0, 1))) * 0.5f; // 수정: 0.5 -> 0.5f
+
+				vec3 V = w;
+				vec3 N(dx, dy, dz), L = w;// (1, 0, 0);
+
+				if (length(N) > 0.0f) N = normalize(N);
+				vec3 H = normalize(L + V);
+				float NL = fabs(dot(N, L));
+				float NH = fabs(dot(N, H));
+
+
+
+				float Ia = 0.25f, Id = 0.5f, Is = 0.9f;  //살짝 밝게 // 합이 1인게 좋은데 여러 표현 가능
+
+
+				glm::vec3 Ka(r * 0.8f, g * 0.8f, b * 0.8f); //주변광 반사율 0.8 곱(어두운 배경 연출) // k = 반사율 = 색상(중요!!!!!! 놀랍군)
+				glm::vec3 Kd(r, g, b);// = c;
+				glm::vec3 Ks(1.2f, 0.8f, 0.8f); //오팔 느낌
+
+				glm::vec3 I = Ia * Ka + Id * Kd * NL + Is * Ks * pow(NH, 30.0f);
+				I = glm::clamp(I, 0.0f, 1.0f);//__min
+
+				r_sum += (1.0f - a_sum) * (I.r * alpha);
+				g_sum += (1.0f - a_sum) * (I.g * alpha);
+				b_sum += (1.0f - a_sum) * (I.b * alpha);
+				a_sum += (1.0f - a_sum) * alpha;
+				if (a_sum > 0.99f) break; // 조기 광선 종료, early ray termination
+
+			}
+			MyTexture[y][x][0] = int(r_sum * 255);
+			MyTexture[y][x][1] = int(g_sum * 255);
+			MyTexture[y][x][2] = int(b_sum * 255);
+
+		}
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	std::cout << "실행 시간: " << duration.count() * 0.001f << " ms" << std::endl;
+}
 extern "C" int cuInit();
 void MyInit() {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	FileRead();
+	GenBlocks(); // 파일은 읽고 난 다음에.
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
@@ -251,6 +447,7 @@ void MyDisplay() {
 }
 
 int main(int argc, char** argv) {
+	//cumain();
 	glutInit(&argc, argv); //GLUT 윈도우 함수
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -259,5 +456,6 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(MyDisplay);
 	glutIdleFunc(MyDisplay);
 	glutMainLoop();
+
 	return 0;
 }
